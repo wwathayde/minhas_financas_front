44651028eed5cfa6c8cf20cb80ca7146380e1db3
Wesley Athayde <wwathayde@gmail.com>
2022-12-14 20:33:40 -0300

    Alterada url para ambiente remoto

--- src/app/apiservice.js
+++ src/app/apiservice.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const httpClient = axios.create({
-    baseURL: 'http://localhost:8080'
-    //baseURL:'https://wesley-financas-backend.herokuapp.com'
+    //baseURL: 'http://localhost:8080'
+    baseURL:'https://wesley-financas-backend.herokuapp.com'
 })
 
 const header = {
